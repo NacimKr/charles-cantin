32400288eaabb75f84abedec627254503a449be2
Nass <nassimkarroum0@gmail.com>
2022-05-30 13:12:47 +0100

    Ajout d'un formulaire fonctionnel pour le projet

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -1,24 +1,48 @@
-import React from 'react'
+
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+
 
 const Contact = () => {
+
+    const [state, handleSubmit] = useForm("xyyoobvz");
+
+    if (state.succeeded) {
+        return <p>Merci pour votre message</p>;
+    }
+
   return (
     <>
-      <form class=" container mt-5">
+      <form onSubmit={handleSubmit} class=" container mt-5" method="POST">
+        
         <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">Votre nom</label>
-          <input type="email" class="form-control bg-light border-secondary" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          <input type="text" class="form-control bg-light border-secondary"  />
+          <ValidationError 
+            prefix="Email" 
+            field="email"
+            errors={state.errors}
+          />
         </div>
+
         <div class="mb-3">
           <label for="exampleInputPassword1" class="form-label bg-light">Votre Email</label>
-          <input type="text" class="form-control bg-light border-secondary" id="exampleInputPassword1" />
+          <input type="email" class="form-control bg-light border-secondary" id="exampleInputPassword1" />
         </div>
 
         <label for="floatingTextarea">Votre message</label>
+
         <div class="form-floating my-3">
-          <textarea class="form-control bg-light border-secondary" placeholder="Leave a comment here" style={{resize:"none", height:"250px"}} id="floatingTextarea" ></textarea>
+          <textarea id="message" name="message" class="form-control bg-light border-secondary" placeholder="Leave a comment here" style={{resize:"none", height:"250px"}} id="floatingTextarea" ></textarea>
+          <ValidationError 
+            prefix="Message" 
+            field="message"
+            errors={state.errors}
+          />
         </div>
 
-        <button type="submit" class="btn btn-secondary d-block mx-auto px-5">Envoyer</button>
+        <button type="submit" disabled={state.submitting} class="btn btn-secondary d-block mx-auto px-5">Envoyer</button>
+
       </form>
     </>
   )
